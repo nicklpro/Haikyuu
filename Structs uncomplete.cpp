75677b90b13cb8f65914d97cb2aa888c9f5d85e6
nicklpro <74418296+nicklpro@users.noreply.github.com>
2020-12-04 16:46:21 +0200

    Add files via upload

---
+++ Structs uncomplete.cpp
@@ -0,0 +1,25 @@
+int main() {
+	// Complete the program
+    string a="abcd";
+    string b="ef";
+    int len = a.size();
+    int len = b.size();
+    
+    //first line
+    cout<<a.size<<"\t"<<b.size<<endl;
+    
+    //second line
+    cout<<a+b<<endl;
+    int temp;
+    char c0=a[0];
+    char c1=b[0];
+    //swap
+    temp=c0;
+    c0=c1;
+    c1=temp;
+    //third line
+    cout<<a+b;
+    
+    
+    return 0;
+
